8a70503b33b600639426896f5b8a11dfd360a1e3
kottojr-blip <kottojr@gmail.com>
2026-05-22 03:37:07 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,58 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-const GallerySchema = new mongoose.Schema({
-  code: String,
-  paid: Boolean,
-  images: [String]
-});
-
-const Gallery = mongoose.model("Gallery", GallerySchema);
-
-app.get("/api/gallery/:code", async (req, res) => {
-
-  const gallery = await Gallery.findOne({
-    code: req.params.code
-  });
-
-  if (!gallery) {
-    return res.status(404).json({
-      message: "Gallery not found"
-    });
-  }
-
-  res.json(gallery);
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
-
-
-const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Image Studio Gallery Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
